3d55d05f08e44b3e9433c198802b0814cd3f0fcb
modastic <admin@modastic.com>
2014-08-22 15:11:00 -0400

    Remove unused function throwing warnings.

--- WYPopoverController/WYPopoverController.m
+++ WYPopoverController/WYPopoverController.m
@@ -2936,29 +2936,6 @@ static WYPopoverTheme *defaultTheme_ = nil;
 
 #pragma mark Inline functions
 
-static NSString* WYStringFromOrientation(NSInteger orientation) {
-    NSString *result = @"Unknown";
-    
-    switch (orientation) {
-        case UIInterfaceOrientationPortrait:
-            result = @"Portrait";
-            break;
-        case UIInterfaceOrientationPortraitUpsideDown:
-            result = @"Portrait UpsideDown";
-            break;
-        case UIInterfaceOrientationLandscapeLeft:
-            result = @"Landscape Left";
-            break;
-        case UIInterfaceOrientationLandscapeRight:
-            result = @"Landscape Right";
-            break;
-        default:
-            break;
-    }
-    
-    return result;
-}
-
 static float WYStatusBarHeight() {
     UIInterfaceOrientation orienation = [[UIApplication sharedApplication] statusBarOrientation];
     
